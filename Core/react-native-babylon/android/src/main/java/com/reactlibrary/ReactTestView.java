07943ff6812e8f94fca15eb5ce8230892a8e2d33
Ryan Tremblay <ryan.tremblay@microsoft.com>
2020-03-20 15:15:24 -0700

    Add GetJsGlobalContextRef.jar to the module package and test out using it

--- Core/react-native-babylon/android/src/main/java/com/reactlibrary/ReactTestView.java
+++ Core/react-native-babylon/android/src/main/java/com/reactlibrary/ReactTestView.java
@@ -4,9 +4,20 @@ import android.content.Context;
 
 import androidx.appcompat.widget.AppCompatTextView;
 
+import com.babylon.GetJsGlobalContextRef;
+import com.facebook.react.bridge.ReactContext;
+
 public class ReactTestView extends AppCompatTextView {
-    public ReactTestView(Context context) {
-        super(context);
+    private final ReactContext reactContext;
+
+    public ReactTestView(ReactContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
         this.setText("this is a native test component");
+
+        GetJsGlobalContextRef getJsGlobalContextRef = new GetJsGlobalContextRef();
+        long address = reactContext.getJavaScriptContextHolder().get();
+        long ptr = getJsGlobalContextRef.GetJsGlobalContextRef(address);
+        System.out.println(ptr);
     }
 }
