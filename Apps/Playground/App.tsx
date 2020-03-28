14563173fe85c6e8bb7f04e9fddf3e0c492d4836
Ryan Tremblay <ryan.tremblay@microsoft.com>
2020-03-27 17:19:44 -0700

    Introducing useEngine custom hook for (contractual) engine initialization and lifetime management
    Remove viewReady event from EngineView since this is managed by the hook
    
    Move threading code into the interop helper
    
    Add a camera prop to EngineView to meet the desired contract, and use it to enable the render loop
    
    Update min SDK version to 24 to enable using CompletableFuture

--- Apps/Playground/App.tsx
+++ Apps/Playground/App.tsx
@@ -8,17 +8,19 @@
 import React, { useState } from 'react';
 import { SafeAreaView, StatusBar, Button, View } from 'react-native';
 
-import { EngineView } from 'react-native-babylon';
-import { Scene, Vector3, Mesh, ArcRotateCamera, Engine } from '@babylonjs/core';
+import { EngineView, useEngine } from 'react-native-babylon';
+import { Scene, Vector3, Mesh, ArcRotateCamera, Engine, Camera } from '@babylonjs/core';
 
 const App = () => {
   const [toggleView, setToggleView] = useState(false);
+  const [camera, setCamera] = useState<Camera>();
 
-  const onEngineViewReady = (engine: Engine) => {
+  useEngine((engine: Engine) => {
     var scene = new Scene(engine);
     scene.createDefaultCamera(true);
     if (scene.activeCamera != null) {
       (scene.activeCamera as ArcRotateCamera).alpha += Math.PI;
+      setCamera(scene.activeCamera);
     }
     scene.createDefaultLight(true);
 
@@ -27,18 +29,18 @@ const App = () => {
     scene.beforeRender = function () {
       scene.meshes[0].rotate(Vector3.Up(), 0.005 * scene.getAnimationRatio());
     };
-  };
+  });
 
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={{flex: 1}}>
         { !toggleView &&
-          <EngineView style={{flex: 1}} onViewReady={onEngineViewReady} />
+          <EngineView style={{flex: 1}} camera={camera} />
         }
         { toggleView &&
           <View style={{flex: 1}}>
-            <EngineView style={{flex: 1}} onViewReady={onEngineViewReady} />
+            <EngineView style={{flex: 1}} camera={camera} />
           </View>
         }
         <Button title="Toggle View Instance" onPress={() => { setToggleView(!toggleView) }} />
